633ae8b896079ab69333ff2038152576c0185284
musfiq180214 <musfiq677@gmail.com>
2026-04-10 22:06:45 +0600

    App Receives Notification when foreground, background, terminated & App Shows pop up when foreground

--- lib/core/firebase_api.dart
+++ lib/core/firebase_api.dart
@@ -1,7 +1,11 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../features/notifications/presentation/notification_screen.dart';
 import '../main.dart';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class FirebaseApi {
   final _firebaseMessaging = FirebaseMessaging.instance;
@@ -13,6 +17,9 @@ class FirebaseApi {
     print("FCM Token: $token");
 
     _setupListeners();
+  }
+  void _handleMessage() {
+
   }
 
   // 🔥 COMMON SAVE FUNCTION (USED EVERYWHERE)
@@ -46,7 +53,9 @@ class FirebaseApi {
 
   void _setupListeners() {
     // ✅ Foreground
+    // ✅ Foreground
     FirebaseMessaging.onMessage.listen((message) {
+      _showTopMessage(message);
       saveToFirestore(message);
     });
 
@@ -57,5 +66,38 @@ class FirebaseApi {
       // 🔥 THIS IS WHAT YOU ARE MISSING
       navigatorKey.currentState?.pushNamed('/notification_screen');
     });
+  }
+
+  void _showTopMessage(RemoteMessage message) {
+    final context = navigatorKey.currentContext;
+    if (context == null) return;
+
+    final title = message.notification?.title ?? "New Notification";
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          title,
+          style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.green,
+        behavior: SnackBarBehavior.floating,
+        margin: const EdgeInsets.only(bottom: 70, left: 20, right: 20),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        duration: const Duration(seconds: 5),
+        action: SnackBarAction(
+          label: 'VIEW',
+          textColor: Colors.white,
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const NotificationScreen(),
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
